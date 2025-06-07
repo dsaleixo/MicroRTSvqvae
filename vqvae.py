f3724cf88141304e2c505495a37cfcc5883d8f33
dsaleixo <david_leixo@hotmail.com>
2025-06-07 16:01:03 -0300

    coloca 0.1

--- vqvae.py
+++ vqvae.py
@@ -207,7 +207,7 @@ class VQVAE(nn.Module):
                 reconstructions, vq_loss, _ = self(x)
                 reconstruction_loss = F.mse_loss(reconstructions, x)
                 loss_jesus = self.closest_palette_loss(reconstructions, x,self.palette)
-                total_loss = loss_jesus+reconstruction_loss+ vq_loss
+                total_loss = loss_jesus+reconstruction_loss*0.1+ vq_loss
                 total_loss.backward()
                 torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=1.0)
                 optimizer.step()
