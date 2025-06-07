1f262f9cad0298c7cfeeb145e050c9a7f88548fd
dsaleixo <david_leixo@hotmail.com>
2025-06-07 16:30:50 -0300

    ff

--- vqvae.py
+++ vqvae.py
@@ -191,7 +191,7 @@ class VQVAE(nn.Module):
 
     def loopTrain(self, max_epochs: int, train_loader: DataLoader, val_loader: DataLoader, device='cuda'):
         self.to(device)
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
 
         for epoch in range(max_epochs):
             self.train()
@@ -207,7 +207,7 @@ class VQVAE(nn.Module):
                 reconstructions, vq_loss, _ = self(x)
                 reconstruction_loss = F.mse_loss(reconstructions, x)
                 loss_jesus = self.closest_palette_loss(reconstructions, x,self.palette)
-                total_loss = loss_jesus+reconstruction_loss*0.1+# vq_loss
+                total_loss = loss_jesus+reconstruction_loss*0.1#+# vq_loss
                 total_loss.backward()
                 #torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=1.0)
                 optimizer.step()
