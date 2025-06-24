abdb3d82784b2e088c422079ad401d4cfb2cc2c4
dsaleixo <david_leixo@hotmail.com>
2025-06-24 15:32:32 -0300

    e la vmaos noss

--- train.py
+++ train.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     test_size = total_size - train_size  # 20 amostras para teste
     train_set, val_set = random_split(datas, [train_size, test_size])
 
-    train_loader = DataLoader(train_set, batch_size=32)
-    val_loader = DataLoader(val_set, batch_size=32, )
+    train_loader = DataLoader(train_set, batch_size=64)
+    val_loader = DataLoader(val_set, batch_size=64, )
     
 
     # Model Parameters
