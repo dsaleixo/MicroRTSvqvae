7fa31ab735a62911b9314fbb8d496bb4d463fa9a
dsaleixo <david_leixo@hotmail.com>
2025-06-24 16:38:03 -0300

    job

---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=meu_job
+#SBATCH --output=saida.log
+#SBATCH --error=erro.log
+#SBATCH --time=00:15:00
+#SBATCH --account=rrg-lelis
+#SBATCH --gres=gpu:a100_2g.10gb:1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=1
+
+source ~/envs/VQVAE/bin/activate
+
+
+python train.py
