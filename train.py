74d2513a02eb0bc62f0bfcf8feee9419711183ed
dsaleixo <david_leixo@hotmail.com>
2025-06-26 00:25:31 -0300

    dd

--- train.py
+++ train.py
@@ -18,15 +18,15 @@ if __name__ == "__main__":
     test_size = total_size - train_size  # 20 amostras para teste
     train_set, val_set = random_split(datas, [train_size, test_size])
 
-    train_loader = DataLoader(train_set, batch_size=128)
-    val_loader = DataLoader(val_set, batch_size=64, )
+    train_loader = DataLoader(train_set, batch_size=160)
+    val_loader = DataLoader(val_set, batch_size=128, )
     
 
     # Model Parameters
     num_hiddens = 64
 
     num_embeddings = 512 # Size of the codebook
-    embedding_dim = 16   # Dimension of each embedding vector
+    embedding_dim = 128   # Dimension of each embedding vector
     commitment_cost = 0.25
 
     # Instantiate the VQ-VAE model
--- vqvae.py
+++ vqvae.py
@@ -75,7 +75,7 @@ class VectorQuantizer(nn.Module):
 
 
 class VectorQuantizerEMA(nn.Module):
-    def __init__(self, num_embeddings: int, embedding_dim: int, decay: float = 0.99, epsilon: float = 1e-5):
+    def __init__(self, num_embeddings: int, embedding_dim: int, decay: float = 0.99, epsilon: float = 1e-3):
         """
         VQ-VAE codebook with Exponential Moving Average (EMA) updates.
 
@@ -462,8 +462,8 @@ class VQVAE(nn.Module):
                 reconstructions, vq_loss, _ = self(x,epoch)
                 reconstruction_loss = F.mse_loss(reconstructions, x)
                 loss_jesus = self.closest_palette_loss(reconstructions, x,self.palette)
-                total_loss = loss_jesus#+reconstruction_loss*0.1#+# vq_loss
-                
+                #total_loss = loss_jesus#+reconstruction_loss*0.1#+# vq_loss
+                total_loss = reconstruction_loss#+# vq_loss
                    
                 total_loss.backward()
                 #torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=1.0)
