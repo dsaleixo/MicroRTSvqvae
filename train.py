53d52402a3f50aa20c7023fc4e439408f2e040c3
dsaleixo <david_leixo@hotmail.com>
2025-07-01 17:49:51 -0300

    jesus

--- train.py
+++ train.py
@@ -180,8 +180,8 @@ def validation(model, val_loader: DataLoader, device='cuda',):
         reconstructions, vq_loss, _,_,_ = model(x,112)
         reconstruction_loss = F.mse_loss(reconstructions, x)
         loss_jesus = closest_palette_loss(reconstructions, x,palette)
-        #total_loss = reconstruction_loss +loss_jesus
-        total_loss = reconstruction_loss# +loss_jesus
+        total_loss = reconstruction_loss +loss_jesus
+        #total_loss = reconstruction_loss# +loss_jesus
         if vq_loss!=None:
             vq_loss_epoch += vq_loss.item()
         
@@ -251,8 +251,8 @@ def loopTrain(model, max_epochs: int, train_loader: DataLoader, val_loader: Data
                 reconstructions, vq_loss, _,perplexity, used_codes = model(x,epoch)
                 reconstruction_loss = F.mse_loss(reconstructions, x)
                 loss_jesus = closest_palette_loss(reconstructions, x,palette)
-                #total_loss = loss_jesus+reconstruction_loss#+# vq_loss
-                total_loss = reconstruction_loss#+vq_loss
+                total_loss = loss_jesus+reconstruction_loss#+# vq_loss
+                #total_loss = reconstruction_loss#+vq_loss
                    
                 total_loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
