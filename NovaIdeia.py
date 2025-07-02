4a8521bb936324d7ba8258ec0eedcebeb758e3e6
dsaleixo <david_leixo@hotmail.com>
2025-07-02 18:20:08 -0300

    tess

--- NovaIdeia.py
+++ NovaIdeia.py
@@ -242,7 +242,7 @@ class NovaIDEIA(nn.Module):
             used_codes = 0
 
         elif epoch <epoch_inicial+transi:
-            self.vq.decay=0.99
+            self.vq.decay=0.999
             quantized, vq_loss, codes, perplexity, used_codes = self.vq(z)
             alpha = (epoch-epoch_inicial )/ transi
             z_mix = (1 - alpha) * z + alpha * quantized
@@ -252,7 +252,7 @@ class NovaIDEIA(nn.Module):
             perplexity = perplexity
             used_codes =  used_codes
         else:
-            self.vq.decay=0.2
+            self.vq.decay=0.99
             quantized, vq_loss, codes, perplexity, used_codes = self.vq(z)
             z_mix = quantized  
 
