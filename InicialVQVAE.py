472b99f61e8d6c3b37ec810b93dc796300810ea1
dsaleixo <david_leixo@hotmail.com>
2025-07-02 15:00:44 -0300

    teste

--- InicialVQVAE.py
+++ InicialVQVAE.py
@@ -24,9 +24,12 @@ class VectorQuantizerEMA(nn.Module):
         self.epsilon = epsilon
 
         # Codebook: shape (M, D)
-        self.register_buffer("embedding", (F.normalize(torch.randn(num_embeddings, embedding_dim), dim=1)*2)-1 )
+        self.register_buffer("embedding", (F.normalize(torch.randn(num_embeddings, embedding_dim), dim=1)))
+        self.embedding[0] = torch.zeros_like(self.embedding[0])
         self.register_buffer("cluster_size", torch.ones(num_embeddings))
         self.register_buffer("embedding_avg", self.embedding.clone())
+
+
 
     def forward(self, z: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         """
