03d9bd4573b060d018449efe1a4daa0a043a7472
dsaleixo <david_leixo@hotmail.com>
2025-07-02 22:40:07 -0300

    nove

--- NovaIdeia.py
+++ NovaIdeia.py
@@ -214,14 +214,14 @@ class VectorQuantizerEMA(nn.Module):
                 cont+=1
         print("0s",cont)
         print()
-    
+    #utils.weight_norm
 import torch.nn.utils as utils
 class Encoder(nn.Module):
     def __init__(self, embedding_dim: int):
         super().__init__()
-        self.conv1 = utils.weight_norm(nn.Conv3d(in_channels=3, out_channels=4, kernel_size=4, stride=2, padding=1))
-        self.conv2 = utils.weight_norm(nn.Conv3d(in_channels=4, out_channels=8, kernel_size=4, stride=2, padding=1))
-        self.conv4 = utils.weight_norm(nn.Conv3d(in_channels=8, out_channels=embedding_dim, kernel_size=3, stride=1, padding=1))
+        self.conv1 = nn.Conv3d(in_channels=3, out_channels=4, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv3d(in_channels=4, out_channels=8, kernel_size=4, stride=2, padding=1)
+        self.conv4 = nn.Conv3d(in_channels=8, out_channels=embedding_dim, kernel_size=3, stride=1, padding=1)
         self.relu = nn.Tanh()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -233,9 +233,9 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, embedding_dim: int):
         super().__init__()
-        self.deconv1 = utils.weight_norm(nn.ConvTranspose3d(in_channels=embedding_dim, out_channels=8, kernel_size=4, stride=2, padding=1))
-        self.deconv3 = utils.weight_norm(nn.ConvTranspose3d(in_channels=8, out_channels=4, kernel_size=4, stride=2, padding=1))
-        self.deconv4 = utils.weight_norm(nn.ConvTranspose3d(in_channels=4, out_channels=3, kernel_size=3, stride=1, padding=1))
+        self.deconv1 = nn.ConvTranspose3d(in_channels=embedding_dim, out_channels=8, kernel_size=4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose3d(in_channels=8, out_channels=4, kernel_size=4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose3d(in_channels=4, out_channels=3, kernel_size=3, stride=1, padding=1)
         self.relu = nn.Tanh()
         self.patial_dropout3d = nn.Dropout3d(p=0.05)
     def forward(self, x: torch.Tensor) -> torch.Tensor:
