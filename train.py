39762568052dc0f4eee386698bc301ab5315af7b
dsaleixo <david_leixo@hotmail.com>
2025-08-26 10:46:39 -0300

    roa

--- train.py
+++ train.py
@@ -390,7 +390,7 @@ if __name__ == "__main__":
    
     wandb.init(
     project="VQVAE",
-    name = "coe32Aam ",
+    name = "coe32AamPesao ",
     config={
          
       
@@ -426,7 +426,7 @@ if __name__ == "__main__":
     test_size = total_size - train_size  # 20 amostras para teste
     train_set, val_set = random_split(datas, [train_size, test_size])
 
-    train_loader = DataLoader(train_set, batch_size=128)
+    train_loader = DataLoader(train_set, batch_size=32)
     val_loader = DataLoader(val_set, batch_size=32, )
  
     exemplo = np.load("resultado.npy")[:sizeVideo]
