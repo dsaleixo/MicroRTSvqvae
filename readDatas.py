7be5db7b3d2e0964594d4e46ca313f4c6e22973a
dsaleixo <david_leixo@hotmail.com>
2025-09-02 10:47:49 -0300

    rem lim rea

--- readDatas.py
+++ readDatas.py
@@ -27,8 +27,7 @@ class ReadDatas():
         print(len(arquivos))
         cont=0
         for arq in arquivos:
-                    if cont > 20 :
-                          continue
+                   
                     cont+=1
                     if cont%100==0:
                           print(cont//100)
