1b003df6aec35e552179d89a402ed72a44890bbd
dsaleixo <david_leixo@hotmail.com>
2025-09-09 11:23:23 -0300

    ff

--- train.py
+++ train.py
@@ -193,7 +193,7 @@ def validation(model, val_loader: DataLoader, device='cuda',):
         reconstructions, vq_loss, _,_,_ = model(x)
         reconstruction_loss = F.mse_loss(reconstructions, x)
         loss_jesus = closest_palette_loss(reconstructions, x,palette)
-        total_loss = loss_jesus+reconstruction_loss +vq_loss*5
+        total_loss = 20*loss_jesus+reconstruction_loss +vq_loss*5
         #total_loss = reconstruction_loss# +loss_jesus
         if vq_loss!=None:
             vq_loss_epoch += vq_loss.item()
@@ -312,7 +312,7 @@ def loopTrain(model, max_epochs: int, train_loader: DataLoader, val_loader: Data
    
                 loss_jesus = closest_palette_loss(reconstructions, x,palette)
                 loss2_norm = loss_jesus #/ loss_jesus.detach().mean()
-                total_loss =   loss_jesus+reconstruction_loss +vq_loss*5
+                total_loss =   20*loss_jesus+reconstruction_loss +vq_loss*5
                 #total_loss = reconstruction_loss#+vq_loss
                    
                 total_loss.backward()
